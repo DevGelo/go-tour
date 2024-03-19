073b5488171b3b7d142586314509ce644374ec35
DevGelo <DevGelo@users.noreply.github.com>
2024-03-19 12:07:32 +0300

    Create 024 Generics - Basics Ex 1.go

---
+++ 024 Generics - Basics Ex 1.go
@@ -0,0 +1,43 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Implement a generic function that can marshal JSON.
+package main
+
+// Add imports.
+import (
+	"fmt"
+	"encoding/json"
+)
+
+// Implement the generic function named marshal that can accept any value
+// of type T and marshal that value into JSON.
+func marshal[T any](v T) ([]byte, error) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	
+	return data, nil
+}
+
+// Declare a struct type named User with two fields, Name and Age.
+type User struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	// Construct a value of type User.
+	jeff := User{"Jeff", 21}
+
+	// Call the generic marshal function.
+	data, err := marshal(jeff)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	// Print the JSON produced by the marshal function.
+	fmt.Println(string(data))
+}
